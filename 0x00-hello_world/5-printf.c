b33b1a67e76d2c07cd141065bb5e20ab5bdb8731
Eflowz <ezekielkachy@gmail.com>
2023-03-18 15:39:17 -0700

    hottie

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - Main function
-* Return: return 0 everytime
-*/   
- *
- *   int main(void)
- *   {
- *   	printf("with proper grammar, but the outcome is a piece of art,\n");
- *
- *   		return (0);
- *
- *   		}
+*main - Main function
+*Return: return 0 everytime
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
