1a690b09a25d891f6665daeb81a73eef2ffdddc4
Eflowz <ezekielkachy@gmail.com>
2023-03-19 13:15:15 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,11 +2,11 @@
   
 /** 
 * main - Entry point
-* Return: Always 0 (Success)
+* Return: Always 0 (Succes)
 */
 
-int main(void) 
- { 
+int main(void)
+{ 
         printf("Size of a char: %i byte(s)\n", sizeof(char));
 	 							
 	 printf("Size of an int: %i byte(s)\n", sizeof(int));
