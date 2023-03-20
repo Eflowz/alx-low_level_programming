b44d5a8b5fa9638271caad98fcde919c48ab1acb
Eflowz <ezekielkachy@gmail.com>
2023-03-20 14:35:49 -0700

    baseof

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,45 +1,22 @@
 #include <stdio.h>
-#include <ctype.h>
+
 /**
- * main - Print all single digits of 16 followed by new line
+ * main - Prints numbers between 0 to 9 and letters betwween a to f.
  *
- * Return: return 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int i;
 
-		int digit;
-
-
-
-			for (digit = '0'; digit <= '9'; digit++)
-
-					{
-
-					putchar(digit);
-			
-					if (digit == '9')
-
-					{
-
-																	digit = 'a';
-
-																	for (; digit <= 'f'; digit++)
-
-																	{
-
-																												putchar(digit);
-
-																													}
-
-																		break;
-
-																			}
-
-						}
-
-				putchar('\n');
-
-					return (0);
-
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
