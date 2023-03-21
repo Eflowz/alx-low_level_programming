f73c09ba625e2fc8c71aee5d04423b5659d6d81b
Eflowz <ezekielkachy@gmail.com>
2023-03-21 14:43:46 -0700

    nature

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,11 +8,11 @@ int main(void)
 {
 		int f, sum = 0;
 
-			for (f = 0; f < 1024; f++)
-					{
-								if ((f % 3) == 0 || (f % 5) == 0)
-											sum += f;
-									}
-				printf("%d\n", sum);
-					return (0);
+		for (f = 0; f < 1024; f++)
+		{
+			if ((f % 3) == 0 || (f % 5) == 0)
+			sum += f;
+		}
+		printf("%d\n", sum);
+		return (0);
 }
