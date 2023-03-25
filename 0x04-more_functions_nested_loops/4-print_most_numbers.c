bec73e974af55412ab1af0aa1ab24d98db1af893
Eflowz <ezekielkachy@gmail.com>
2023-03-24 17:19:40 -0700

    most num

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - Entry point
+ * Description: prints 0 to 9
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i = 48;
+	while (i <= 57)
+	{
+		if (i != 50 && i != 52)
+		{
+			putchar((char)i);
+		}
+		i++;
+	}
+	putchar((char)'\n');
+}
