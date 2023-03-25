01f2d8882989cff2d5b9e2cef832754f1c06044f
Eflowz <ezekielkachy@gmail.com>
2023-03-24 17:15:14 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ * Description: prints 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = 48;
+	while (i <= 57)
+	{
+		_putchar((char) i);
+		i++;
+	}
+	_putchar((char)'\n');
+}
