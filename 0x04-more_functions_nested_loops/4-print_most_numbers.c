f11128e266d0f2d214329c42df11348c5db70ec0
Eflowz <ezekielkachy@gmail.com>
2023-03-24 19:02:48 -0700

    3numba

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,11 @@ void print_most_numbers(void)
 	int i = 48;
 	while (i <= 57)
 	{
-		if (i != 50 && i != 52)
-		{
-			putchar((char)i);
-		}
-		i++;
+	if (i != 50 && i != 52)
+	{
+	putchar((char)i);
+	}
+	i++;
 	}
 	putchar((char)'\n');
 }
