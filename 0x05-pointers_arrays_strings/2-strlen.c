70e99c560e74cc938a7f5d0f07993ab1ab1ffe38
Eflowz <ezekielkachy@gmail.com>
2023-03-27 20:40:16 -0700

    report

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - length of a string
+ * @c: pointer parameter a
+ * Description: length  pointer value
+ * Return: integer
+ */
+
+int _strlen(char *c)
+{
+	int i = 0;
+	for (; c[i] != '\0';)
+	{
+i++;
+	}
+	return (i);
+}
