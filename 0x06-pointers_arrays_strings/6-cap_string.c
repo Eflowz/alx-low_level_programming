53582f0add9839d7b7396b8f18b03787b6403ed2
Eflowz <ezekielkachy@gmail.com>
2023-03-30 08:55:41 -0700

    hitfix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -25,13 +25,13 @@ char *cap_string(char *s)
 
 		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
 
-				    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+				|| s[count] == ',' || s[count] == ';' || s[count] == '.'
 
-				        || s[count] == '.' || s[count] == '!' || s[count] == '?'
+				|| s[count] == '.' || s[count] == '!' || s[count] == '?'
 
-					    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+				|| s[count] == '"' || s[count] == '(' || s[count] == ')'
 
-					        || s[count] == '{' || s[count] == '}')
+				|| s[count] == '{' || s[count] == '}')
 		{
 			if (s[count + 1] >= 97 && s[count + 1] <= 122)
 			{
